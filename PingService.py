060b1731cbaf7ecc06584e83d3a6c82eb7678603
Max McCulloch <max.mcculloch.prof@gmail.com>
2021-10-10 19:29:17 -0400

    fixed import.3

--- PingService.py
+++ PingService.py
@@ -1,24 +1,19 @@
 import os
+import json
 from flask import Flask, render_template, jsonify
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from Flask_HTTPAuth import HTTPDigestAuth
+import requests
+from requests.auth import HTTPDigestAuth
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-
-auth = HTTPDigestAuth()
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
 @app.route('/ping', methods=['GET'])
-@auth.login_required()
 def ping():
     url = 'https://mccullochme-pong.herokuapp.com/pong'
     request = requests.get(url, auth=HTTPDigestAuth('vcu', 'rams'))
