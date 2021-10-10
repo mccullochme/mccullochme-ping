e172ee6b1d8af0f3e7bd09ef1b21490ec9000853
Max McCulloch <max.mcculloch.prof@gmail.com>
2021-10-10 17:53:00 -0400

    fixed issues

--- PingService.py
+++ PingService.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import requests
 from requests.auth import HTTPDigestAuth
 
@@ -8,13 +10,19 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/', methods=[GET, POST])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-@app.route('/ping', methods=[GET])
+@app.route('/ping', methods=['GET'])
 def ping():
     #create url
     #make request
     #return JSON payload with time request took
-    return '<h1> hello <h1>'
+    return '<h1> hello </h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
